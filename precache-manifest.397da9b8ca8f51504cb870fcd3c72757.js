a9e54316f75c2f2b93040a857a1dde32dd616dcb
Admin <you@example.com>
2020-02-26 02:20:38 +0300

    Updates

---
+++ precache-manifest.397da9b8ca8f51504cb870fcd3c72757.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "332f908187dfcf03ff459316e2eac177",
+    "url": "/test-avito/index.html"
+  },
+  {
+    "revision": "1112604a22af55c6f7a2",
+    "url": "/test-avito/static/css/main.0e901b01.chunk.css"
+  },
+  {
+    "revision": "02a9dfe09a116b1943d7",
+    "url": "/test-avito/static/js/2.fd76bcdf.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/test-avito/static/js/2.fd76bcdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1112604a22af55c6f7a2",
+    "url": "/test-avito/static/js/main.015a1ee4.chunk.js"
+  },
+  {
+    "revision": "13dcef0a70a8ecf3c751",
+    "url": "/test-avito/static/js/runtime-main.d159f565.js"
+  },
+  {
+    "revision": "79a8a2f36060808d542ec2643a2daeda",
+    "url": "/test-avito/static/media/error-image.79a8a2f3.png"
+  }
+]);
